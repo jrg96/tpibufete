36aedab0c9225bf8d97b22beccf3d8ef22193c13
jrg96 <gomezlopez.jorge96@gmail.com>
2017-11-26 15:06:37 -0600

    fix nombre 2

--- application/controllers/ChatGeneral.php
+++ application/controllers/ChatGeneral.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 ////////////////////////////////////TIPO: Pagina Usuario comun ////////////////////
-class ChatGeneral_model extends CI_Controller {
+class ChatGeneral extends CI_Controller {
 
     public function __construct()
     {
